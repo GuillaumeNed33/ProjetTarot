2723a2636d35355f09a00fcc6df093f7c058276c
Guillaume <Guillaume@192.168.1.43>
2016-10-23 23:43:12 +0200

    distribution des cartes

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import Model.CardType;
 
@@ -13,6 +14,8 @@ public class Game {
 	private List<Player> players;
 	private List<Card> cards;
 	private Chien chien;
+	private Team laPrise;
+	private Team laGarde;	
 
 	public Game()
 	{
@@ -28,7 +31,7 @@ public class Game {
 		for(int i=1; i<NBCARDS; i++)
 		{
 			CardType copy = new CardType(type);
-			
+
 			cards.add(new Card(copy, new CardValue(val)));
 			val++;
 
@@ -47,11 +50,11 @@ public class Game {
 
 		type.changeToExcuse();
 		cards.add(new Card(type, new CardValue(0)));	
-		
-		for(Card e : cards)
+
+		/*for(Card e : cards)
 		{
 			System.out.println(e.getType().toString() + ", " + e.getValue().getVal());
-		}
+		}*/
 	}
 
 	public void initGame()
@@ -65,7 +68,38 @@ public class Game {
 
 	public void distribCard()
 	{
-		System.out.println("====== DISTRIBUTION DES CARTES ======");
+		chien = new Chien();
+
+		int i = 0;
+		while(!cards.isEmpty())
+		{
+			Random r = new Random();
+			int index = 0 + r.nextInt(cards.size() - 0);
+			if(i>3)
+			{
+				if(chien.size()<6)
+				{
+					chien.addCard(cards.get(index));
+					cards.remove(index);
+				}
+				i=0;	
+			}
+			else
+			{
+				players.get(i).getHand().addCard(cards.get(index));
+				cards.remove(index);
+				i++;
+			}
+		}
+
+		/*i=1;
+		for(Player p : players)
+		{
+			System.out.println("J" + i + " : " + p.getHand().size());
+			i++;
+		}
+
+		System.out.println("Chien : " + chien.size());*/
 	}
 
 	public void nextStep()
--- src/Model/Hand.java
+++ src/Model/Hand.java
@@ -1,7 +1,33 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
 	private List<Card> playerGame;
+	
+	public Hand()
+	{
+		playerGame = new ArrayList<Card>();
+	}
+	
+	public List<Card> getGame()
+	{
+		return playerGame;
+	}
+	
+	public Card getCard(int index)
+	{
+		return playerGame.get(index);
+	}
+	
+	public void addCard(Card c)
+	{
+		playerGame.add(c);
+	}
+	
+	public int size()
+	{
+		return playerGame.size();
+	}
 }
