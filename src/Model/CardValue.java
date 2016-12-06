a305cbf017581e390a54058b2c8e1be01b3cf78c
Normand Benjamin <Normand Benjamin@DESKTOP-3I352BT>
2016-12-07 00:01:19 +0100

    Commentaire Complet de CardValue et Chien

--- src/Model/CardValue.java
+++ src/Model/CardValue.java
@@ -3,13 +3,44 @@ package Model;
 public class CardValue {
 	private int value;
 	
+	/**
+	 * <i> <b> Constructeur de CardValue </b> </i><br>
+	 * <br>
+	 * <code> CardValue(int v) </code> <br>
+	 * 
+	 * <p> Construit CardValue à l'aide d'une valeur.
+	 * </p>
+	 * 
+	 * @param v : value de CardValue
+	 */
 	CardValue(int v) {
 		this.value = v;
 	}
 	
+	/**
+	 * <i> <b> getValue </b> </i><br>
+	 * <br>
+	 * <code> public int getValue() </code> <br>
+	 * 
+	 * <p> Retourne la valeur de CardValue.
+	 * </p>
+	 * 
+	 * @return Value de CardValue
+	 */
 	public int getValue() {
 		return value;
 	}
+	
+	/**
+	 * <i> <b> setValue </b> </i><br>
+	 * <br>
+	 * <code> public void setValue(int v) </code> <br>
+	 * 
+	 * <p> Permet de préciser la valeur de CardValue.
+	 * </p>
+	 * 
+	 * @param v : value de CardValue
+	 */
 	public void setValue(int v) {
 		this.value = v;
 	}
--- src/Model/Chien.java
+++ src/Model/Chien.java
@@ -1,25 +1,66 @@
 package Model;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Chien {
-	private LinkedList<Card> cards;
+	private ArrayList<Card> cards;
 	
+	/**
+	 * <i> <b> Constructeur de la classe Chien </b> </i><br>
+	 * <br>
+	 * <code> public Chien() </code> <br>
+	 * 
+	 * <p> Construit un Chien vide.
+	 * </p>
+	 * 
+	 */
 	public Chien()
 	{
-		cards = new LinkedList<Card>();
+		cards = new ArrayList<Card>();
 	}
 	
+	/**
+	 * <i> <b> addCard </b> </i><br>
+	 * <br>
+	 * <code> public void addCard(Card c) </code> <br>
+	 * 
+	 * <p> Ajoute une carte au Chien.
+	 * </p>
+	 * @param c : Card à ajouter.
+	 * 
+	 */
 	public void addCard(Card c)
 	{
 		cards.add(c);
 	}
 	
+	/**
+	 * <i> <b> size </b> </i><br>
+	 * <br>
+	 * <code> public int size() </code> <br>
+	 * 
+	 * <p> Renvois le nombre de carte du Chien.
+	 * </p>
+	 * @return Nombre de Card.
+	 * 
+	 */
 	public int size()
 	{
 		return cards.size();
 	}
-	public LinkedList<Card> getCards() {
+	
+	/**
+	 * <i> <b> getCards </b> </i><br>
+	 * <br>
+	 * <code> public ArrayList getCards() </code> <br>
+	 * 
+	 * <p> Retourne la liste de carte du chien
+	 * </p>
+	 * 
+	 * @return ArrayList de Card.
+	 * 
+	 */
+	public ArrayList<Card> getCards() {
 		return cards;
 	}
 }
