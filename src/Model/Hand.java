ed6ab9abfefa13aa4b2ebd2974a0623b76ddc17d
Normand Benjamin <Normand Benjamin@DESKTOP-3I352BT>
2016-12-07 00:10:58 +0100

    Commentaire de Hand et Player

--- src/Model/Hand.java
+++ src/Model/Hand.java
@@ -9,26 +9,75 @@ import java.util.Queue;
 public class Hand {
 	private ArrayList<Card> playerGame;
 	
+	/**
+	 * <i> <b> Constructeur de Hand </b> </i><br>
+	 * <br>
+	 * <code> public Hand() </code> <br>
+	 * 
+	 * <p> Créer une main vide.
+	 * </p>
+	 * 
+	 */
 	public Hand()
 	{
 		playerGame = new ArrayList<Card>();
 	}
 	
+	/**
+	 * <i> <b> getGame </b> </i><br>
+	 * <br>
+	 * <code> public ArrayList getGame() </code> <br>
+	 * 
+	 * <p> Retourne la jeu de carte de la main.
+	 * </p>
+	 * @return ArrayList de Card.
+	 * 
+	 */
 	public ArrayList<Card> getGame()
 	{
 		return playerGame;
 	}
 	
+	/**
+	 * <i> <b> getCard </b> </i><br>
+	 * <br>
+	 * <code> public Card getCard(int index) </code> <br>
+	 * 
+	 * <p> Récupère la carte à la position index.
+	 * </p>
+	 * @param index : Index de la carte dans la Main.
+	 * @return Card à l'index.
+	 */
 	public Card getCard(int index)
 	{
 		return playerGame.get(index);
 	}
 	
+	/**
+	 * <i> <b> addCard </b> </i><br>
+	 * <br>
+	 * <code> public void addCard(Card c) </code> <br>
+	 * 
+	 * <p> Ajoute une carte a la main.
+	 * </p>
+	 * @param c : Card à ajouter.
+	 * 
+	 */
 	public void addCard(Card c)
 	{
 		playerGame.add(c);
 	}
 	
+	/**
+	 * <i> <b> size </b> </i><br>
+	 * <br>
+	 * <code> public int size() </code> <br>
+	 * 
+	 * <p> Retourne le nombre de carte de la main.
+	 * </p>
+	 * @return Nombre de Card.
+	 * 
+	 */
 	public int size()
 	{
 		return playerGame.size();
--- src/Model/Player.java
+++ src/Model/Player.java
@@ -4,17 +4,46 @@ public class Player {
 	private int id;
 	private Hand handGame;  
 	
+	/**
+	 * <i> <b> Constructeur de Player </b> </i><br>
+	 * <br>
+	 * <code> public Player(int id) </code> <br>
+	 * 
+	 * <p> Créer un joueur à partir de son identifiant.
+	 * </p>
+	 * @param id : Identifiant du joueur
+	 */
 	public Player(int id) 
 	{
 		this.id = id;
 		handGame = new Hand();
 	}
 	
+	/**
+	 * <i> <b> getHand </b> </i><br>
+	 * <br>
+	 * <code> public Hand getHand() </code> <br>
+	 * 
+	 * <p> Retourne la main du joueur.
+	 * </p>
+	 * 
+	 * @return Hand du Player.
+	 */
 	public Hand getHand() 
 	{
 		return handGame;
 	}
 	
+	/**
+	 * <i> <b> getId </b> </i><br>
+	 * <br>
+	 * <code> public int getId() </code> <br>
+	 * 
+	 * <p> Retourne l'ID du Joueur.
+	 * </p>
+	 * 
+	 * @return ID de Player
+	 */
 	public int getId()
 	{
 		return id;
