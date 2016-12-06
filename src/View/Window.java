77479405eb6f0927bfb28f0d87495e6d49096369
Guillaume <Guillaume@192.168.1.43>
2016-12-06 23:57:52 +0100

    Merge

--- src/View/Window.java
+++ src/View/Window.java
@@ -478,7 +478,7 @@ public class Window extends Application implements Observer {
 				cV.openDragAndDrop(dropTarget, chienCards);
 			}
 			cV.allowZoom(false);
-		}
+		} 
 	}
 
 	private int allowAtoutToBeDrag() {
